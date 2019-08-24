084b0904de3d0417f5919f44339d8dc10f236be6
Monrad <maj@spiir.dk>
2019-08-24 10:00:24 +0200

    use paging

--- Web/Models/Viia/TransactionsResponse.cs
+++ Web/Models/Viia/TransactionsResponse.cs
@@ -5,6 +5,6 @@ namespace ViiaSample.Models.Viia
     public class TransactionsResponse
     {
         public List<Transaction> Transactions { get; set; }
-        public string ContinuationToken { get; set; }
+        public string PagingToken { get; set; }
     }
 }
