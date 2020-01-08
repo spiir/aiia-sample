0558210c655f56c3a455c46a783be46781fb3622
Edgar <eda@spiir.dk>
2020-01-08 10:05:38 +0100

    Add ability to preselect bank provider

---
+++ Web/Models/Viia/BankProvider.cs
@@ -0,0 +1,9 @@
+namespace ViiaSample.Models.Viia
+{
+    public class BankProvider
+    {
+        public string CountryCode { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
