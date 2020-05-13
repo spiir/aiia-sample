14f4180059b184d1db5fa53ac02b856ce22b20ea
Monrad <maj@spiir.dk>
2020-05-13 21:35:20 +0200

    Delete not used folder

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ViiaSample.Data;
 using ViiaSample.Models;
 
 namespace ViiaSample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _dbContext;
-
-        public HomeController(ApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
