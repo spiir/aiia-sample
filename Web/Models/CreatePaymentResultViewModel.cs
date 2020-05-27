b0450846f46114b62fdcf75e97575ac699789896
Edgaras Ausvicas <eda@spiir.dk>
2020-05-27 10:11:43 +0200

    Minor error handling

---
+++ Web/Models/CreatePaymentResultViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViiaSample.Models
+{
+    public class CreatePaymentResultViewModel
+    {
+        public string PaymentId { get; set; }
+        public string PaymentUrl { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+}
