b97e7b11bb579f299519b63d73f7f80e08053301
Krzysztof Łaputa <krl@aiia.eu>
2022-03-30 12:50:16 +0200

    Match the new Payer Token API

--- Web/Models/Aiia/PayerTokenModel.cs
+++ Web/Models/Aiia/PayerTokenModel.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Aiia.Sample.Models.Aiia
 {
-    public class PayerToken
+    public class PayerTokenModel
     {
         public string ProviderId { get; set; }
         public string RedactedAccountNumber { get; set; }
         public DateTime Expires { get; set; }
-        public string Token { get; set; }
+        public string PayerToken { get; set; }
     }
 }
