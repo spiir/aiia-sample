9da51f51790777b02d97d1b57e1d39b70dd3985e
Krzysztof Łaputa <krl@aiia.eu>
2022-04-07 12:13:31 +0200

    Fix Iban model parsing error

--- Web/Models/Aiia/PaymentReference.cs
+++ Web/Models/Aiia/PaymentReference.cs
@@ -3,7 +3,7 @@ namespace Aiia.Sample.Models.Aiia
     public class PaymentReference
     {
         public BbanParsed Bban { get; set; }
-        public string Iban { get; set; }
+        public IbanParsed Iban { get; set; }
         public string Name { get; set; }
         public InpaymentFormDestinationParsed InpaymentForm { get; set; }
         public PaymentAddressRequest Address { get; set; }
