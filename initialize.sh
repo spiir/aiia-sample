b04e0b5af4bd3116f2d6feb2aa996c89ca658506
Daniele Campana <dac@spiir.dk>
2022-06-30 13:25:18 +0200

    Add initialization scripts so that the DB & secrets are not committed to the repo

---
+++ initialize.sh
@@ -0,0 +1,19 @@
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$ROOT/Web"
+
+echo "Envirnoment configuration script."
+
+cp aiia.db.template aiia.db
+echo "Reinitialized the Sqlite database 'aiia.db' from the template"
+
+echo "We need to configure your clientId and secret to Aiia."
+echo "Please enter them now and we'll set them up for you using 'dotnet user-secrets' (they will not appear in the configuration files)"
+
+echo -n "ClientId: "
+read -r clientId
+echo -n "Secret  : "
+read -r clientSecret
+
+dotnet user-secrets set "Aiia:ClientId" "$clientId"
+dotnet user-secrets set "Aiia:ClientSecret" "$clientSecret"
