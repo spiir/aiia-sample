99664707f2071b573b0d1f2f43a5786588c17e69
Daniele Campana <dac@spiir.dk>
2022-07-05 18:03:06 +0200

    Update EFcore to .net 6 version. Update project language to .net 6

--- Web/Data/Migrations/20220705154906_AddWebhooksStore.Designer.cs
+++ Web/Data/Migrations/20220705154906_AddWebhooksStore.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Aiia.Sample.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aiia.Sample.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220705154906_AddWebhooksStore")]
+    partial class AddWebhooksStore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Web/Data/Migrations/20220705154906_AddWebhooksStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aiia.Sample.Data.Migrations
+{
+    public partial class AddWebhooksStore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable("Webhooks", columns =>
+                new
+                {
+                    Id = columns.Column<ulong>(),
+                    Received = columns.Column<DateTime>(),
+                    UserId = columns.Column<Guid>(),
+                    DataAsJson = columns.Column<string>()
+                }, constraints: constraints =>
+            {
+                constraints.PrimaryKey("PK_Id", columns: x => x.Id);
+            });
+            
+            migrationBuilder.CreateIndex("IDX_Webhooks_UserId", "Webhooks", "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex("IDX_Webhooks_UserId");
+            migrationBuilder.DropTable("Webhooks");
+        }
+    }
+}
