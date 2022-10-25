041726d56b93e04cc45068bbc2f5b0c57355f6d2
Martin Andersen <martin@andersen.ninja>
2022-10-25 15:55:17 +0200

    Removed server header from all http responses

--- Web/Program.cs
+++ Web/Program.cs
@@ -9,11 +9,17 @@ public class Program
 {
     public static IHostBuilder CreateHostBuilder(string[] args)
     {
-        return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(options =>
-        {
-            options.UseStartup<Startup>().UseKeyVault()
-                .UseSerilogElasticSearchIngest();
-        });
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseKestrel(options =>
+                    {
+                        options.AddServerHeader = false;
+                    })
+                    .UseStartup<Startup>()
+                    .UseKeyVault()
+                    .UseSerilogElasticSearchIngest();
+            });
     }
 
     public static async Task Main(string[] args)
