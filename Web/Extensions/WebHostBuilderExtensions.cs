ef690498e7b188b07841f745cef6585ebb530adf
Laur <marius.stoica@mastercard.com>
2023-06-22 15:32:15 +0300

    - updated multiple packages; - replaced depricated package Microsoft.Extensions.Configuration.AzureKeyVault with Azure.Extensions.AspNetCore.Configuration.Secrets; - improved navbar allignment and centered some pages

--- Web/Extensions/WebHostBuilderExtensions.cs
+++ Web/Extensions/WebHostBuilderExtensions.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Reflection;
 using System.Threading;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -23,16 +23,9 @@ public static class WebHostBuilderExtensions
             if (builtConfig["KEY_VAULT_NAME"]
                 .IsSet())
             {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                var keyVaultClient = new KeyVaultClient(
-                    new KeyVaultClient.AuthenticationCallback(
-                        azureServiceTokenProvider
-                            .KeyVaultTokenCallback));
-
-                config.AddAzureKeyVault(
-                    $"https://{builtConfig["KEY_VAULT_NAME"]}.vault.azure.net/",
-                    keyVaultClient,
-                    new DefaultKeyVaultSecretManager());
+                var secretClient = new SecretClient(new Uri($"https://{builtConfig["KEY_VAULT_NAME"]}.vault.azure.net/"),
+                                                         new DefaultAzureCredential());
+                config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
             }
         });
     }
